1a917af25e999c5a94081e994d5ba13d9554b881
R-C0DE <67498302+R-C0DE@users.noreply.github.com>
2021-10-08 15:39:36 +0530

    Update TwoPlayers.py

--- TwoPlayers.py
+++ TwoPlayers.py
@@ -1,4 +1,52 @@
 from functions import *
+import sys
+from random import randint
+import sys
+board={'a1': ' ','a2': ' ','a3': ' ','b1': ' ','b2': ' ','b3': ' ','c1': ' ','c2': ' ','c3': ' '}
+
+def pBoard(Board):
+    print(Board['a1'], ' | ',Board['a2'], ' | ',Board['a3'], )
+    print('---|-----|---')
+    print(Board['b1'], ' | ',Board['b2'], ' | ',Board['b3'], )
+    print('---|-----|---')
+    print(Board['c1'], ' | ',Board['c2'], ' | ',Board['c3'], )
+
+p1=input("Enter the name of player 1 :")
+print(p1, 'will mark X')
+p2=input("Enter the name of player 2 :")
+print(p2, 'will mark 0')
+check='X'
+for i in range(9):
+    while True:
+        choice=input('Enter :')
+        if choice not in board.keys():
+            continue
+        else:
+            if board[choice]==' ':
+                break
+            else:
+                continue
+        
+    if choice in board.keys():
+        if board[choice]==' ':
+            
+            board[choice]=check
+            if check=='X':
+                check='0'
+            else:
+                check='X'
+            if board['a1']==board['a2']==board['a3']=='X' or board['b1']==board['b2']==board['b3']=='X'or board['c1']==board['c2']==board['c3']=='X' or board['a1']==board['b2']==board['c3']=='X' or board['a3']==board['b2']==board['c1']=='X' or board['a1']==board['b1']==board['c1']=='X' or board['a2']==board['b2']==board['c2']=='X':
+                pBoard(board)
+                print(p1, 'won the game')
+                end=input("Press 'Enter' to quit")
+                sys.exit()
+            if board['a1']==board['a2']==board['a3']=='0' or board['b1']==board['b2']==board['b3']=='0'or board['c1']==board['c2']==board['c3']=='0' or board['a1']==board['b2']==board['c3']=='0' or board['a3']==board['b2']==board['c1']=='0' or board['a1']==board['b1']==board['c1']=='0' or board['a2']==board['b2']==board['c2']=='0':
+                pBoard(board)
+                print(p2, ' won the game')
+                end=input("Press'Enter' to quit")
+
+                sys.exit()
+    pBoard(board)
 
 def imgBlitter(x,y,size_x,size_y,path):
 	Img=pygame.image.load(path)
@@ -9,6 +57,63 @@ def drawText(x,y,color,size,text):
 	text_render= font.render(text, True, color)
 	screen.blit(text_render, (x, y))
 
+def screen_size_adjustments(x,y,z):
+        screen_x=x*z
+        screen_y=y*z
+class guess_for_it:
+    def __init__(self, number, mn=0, mx=100):
+        self.number=number
+        self.min=mn
+        self.max=mx
+        self.guesses=0
+    def get_guess(self):
+        guess=input("Enter a guess :")
+        if self.valid_number(guess):
+            return int(guess)
+        else:
+            print("Please enter a valid number :")
+            return self.get_guess()
+    def valid_number(self, ctr):
+        try:
+            number=int(ctr)
+        except:
+            return False
+        return self.min <= number <= self.max
+    def play(self):
+        while True:
+            self.guesses+=1
+            guess=self.get_guess()
+            if guess<self.number:
+                print("your guess was under")
+            elif guess> self.number:
+                print("your guess was over")
+            else:
+                break
+        print(f"____you guessed it in {self.guesses} guesses.____")
+        print()
+#game is ready already. Just two steps more...
+#    game=guess_for_it(random_number, minimum_range, maximum_range)
+#    game.play()
+
+def play_game():
+    minimum_range=int(input("Enter the minimum range : "))
+    maximum_range=int(input("Enter the maximum range : "))
+    random_number=randint(minimum_range, maximum_range)
+    game=guess_for_it(random_number, minimum_range, maximum_range)
+    game.play()
+    play_again=input("Enter 'p or 'P' to play again : ")
+    if play_again=='p' or play_again=='P':
+        play_game()
+    else:
+        print("EXITING")
+n=input("Enter 'p' or 'P' to play : ")
+if n=='p' or n=='P':
+    play_game()
+else:
+    print("EXITING")
+    
+
+        
 def twoPlayer(theme):
 	board={'a1':' ', 'a2':' ','a3':' ','b1':' ','b2':' ','b3':' ','c1':' ', 'c2':' ', 'c3':' '}
 	click=False
@@ -178,3 +283,155 @@ def twoPlayer(theme):
 			playable=False
 			drawText(screen_x//3,screen_x//3,(240,240,240),100,'Tie!')
 		pygame.display.update()
+def emorse(str1):
+    global str2
+    str2=''
+    dict1={'A':".-",
+           'B':"-...",
+           "C":"-.-.",
+           "D":"-..",
+           "E":".",
+           "F":"..-.",
+           "G":"--.",
+           "H":"....",
+           "I":"..",
+           "J":".---",
+           "K":"-.-",
+           "L":".-..",
+           "M":"--",
+           "N":"-.",
+           "O":"---",
+           "P":".--.",
+           "Q":"--.-",
+           "R":".-.",
+           "S":"...",
+           "T":"-",
+           "U":"..-",
+           "V":"...-",
+           "W":".--",
+           "X":"-..-",
+           "Y":"-.--",
+           'Z':'--..'}
+    str3=str1.upper()
+    for i in str3:
+        if i in dict1:
+            str2+=dict1[i]
+            str2+=' '
+        else:
+            str2+=i
+    return str2
+
+def dmorse(str1):
+    str2=''
+    dict1={'A':".-",
+           'B':"-...",
+           "C":"-.-.",
+           "D":"-..",
+           "E":".",
+           "F":"..-.",
+           "G":"--.",
+           "H":"....",
+           "I":"..",
+           "J":".---",
+           "K":"-.-",
+           "L":".-..",
+           "M":"--",
+           "N":"-.",
+           "O":"---",
+           "P":".--.",
+           "Q":"--.-",
+           "R":".-.",
+           "S":"...",
+           "T":"-",
+           "U":"..-",
+           "V":"...-",
+           "W":".--",
+           "X":"-..-",
+           "Y":"-.--",
+           'Z':'--..'}
+    key_list = list(dict1.keys())
+    val_list = list(dict1.values())
+    list1=str1.split(' ')
+    print(list1)
+    for i in list1:
+        if i in val_list:
+            position = val_list.index(i)
+            str3=key_list[position]
+            str2+=str3
+        elif i=='':
+            str2+=' '
+        else:
+            str2+=i
+    return str2
+def rot13cipher(str1):
+    str1.lower()
+    str2=''
+    dict1={'a':'n',
+           'b':'o',
+           'c':'p',
+           'd':'q',
+           'e':'r',
+           'f':'s',
+           'g':'t',
+           'h':'u',
+           'i':'v',
+           'j':'w',
+           'k':'x',
+           'l':'y',
+           'm':'z',
+           'n':'a',
+           'o':'b',
+           'p':'c',
+           'q':'d',
+           'r':'e',
+           's':'f',
+           't':'g',
+           'u':'h',
+           'v':'i',
+           'w':'j',
+           'x':'k',
+           'y':'l',
+           'z':'m'}
+    for i in str1:
+        if i in dict1:
+            str2+=dict1[i]
+        else:
+            str2+=i
+    str3=str2.upper()
+    return str3
+def slapgif(num):
+    list1=["https://tenor.com/view/anime-slap-slapping-hit-mad-gif-17897236",
+           "https://tenor.com/view/anime-manga-japanese-anime-japanese-manga-toradora-gif-5373994",
+           "https://tenor.com/view/mad-angry-pissed-upset-slap-gif-4436362",
+           "https://tenor.com/view/anime-slap-relationship-gif-5463215",
+           "https://tenor.com/view/no-angry-anime-slap-gif-7355956",
+           "https://tenor.com/view/powerful-head-slap-anime-death-tragic-gif-14358509"]
+    return list1[num]
+
+def spankgif(num):
+    list1=["https://tenor.com/view/anime-slap-slapping-hit-mad-gif-17897236",
+           "https://tenor.com/view/anime-manga-japanese-anime-japanese-manga-toradora-gif-5373994",
+           "https://tenor.com/view/mad-angry-pissed-upset-slap-gif-4436362",
+           "https://tenor.com/view/anime-slap-relationship-gif-5463215",
+           "https://tenor.com/view/no-angry-anime-slap-gif-7355956",
+           "https://tenor.com/view/powerful-head-slap-anime-death-tragic-gif-14358509"]
+    return list1[num]
+    
+def huggif(num):
+    list1=["https://tenor.com/view/anime-slap-slapping-hit-mad-gif-17897236",
+           "https://tenor.com/view/anime-manga-japanese-anime-japanese-manga-toradora-gif-5373994",
+           "https://tenor.com/view/mad-angry-pissed-upset-slap-gif-4436362",
+           "https://tenor.com/view/anime-slap-relationship-gif-5463215",
+           "https://tenor.com/view/no-angry-anime-slap-gif-7355956",
+           "https://tenor.com/view/powerful-head-slap-anime-death-tragic-gif-14358509"]
+    return list1[num]
+
+def kissgif(num):
+    list1=["https://tenor.com/view/anime-slap-slapping-hit-mad-gif-17897236",
+           "https://tenor.com/view/anime-manga-japanese-anime-japanese-manga-toradora-gif-5373994",
+           "https://tenor.com/view/mad-angry-pissed-upset-slap-gif-4436362",
+           "https://tenor.com/view/anime-slap-relationship-gif-5463215",
+           "https://tenor.com/view/no-angry-anime-slap-gif-7355956",
+           "https://tenor.com/view/powerful-head-slap-anime-death-tragic-gif-14358509"]
+    return list1[num]
+
